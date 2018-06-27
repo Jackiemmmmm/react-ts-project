b2d799425eb7b054ebc5b1084e87b53b98f6bfe2
Jackiemmmmm <282301059@qq.com>
2018-06-27 16:50:12 +0800

    init: base react-scripts-ts initial project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public state = {
+    test: 1,
+  }
+  public render() {
+    const { test } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+          {test}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
